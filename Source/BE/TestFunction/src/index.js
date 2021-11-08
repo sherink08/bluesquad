e7c091d5cbd4294aeca7189758927fe05fe8c2c3
Muhsin PK <muhsinwazza@gmail.com>
2021-11-08 14:54:11 +0530

    test method update
    updated test method to access dynamodb items

--- Source/BE/TestFunction/src/index.js
+++ Source/BE/TestFunction/src/index.js
@@ -1,7 +1,10 @@
-exports.handler = async (event) => {
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+exports.handler = async function (event)  {
+    var data = await dynamo.scan({ TableName: "test" }).promise();
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: data,
     };
     return response;
 };
