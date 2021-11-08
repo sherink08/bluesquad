a34d8aaa2a22f2bae194d1636f3793805f361ce4
Muhsin PK <muhsinwazza@gmail.com>
2021-11-08 15:17:56 +0530

    updation

--- Source/BE/TestFunction/src/index.js
+++ Source/BE/TestFunction/src/index.js
@@ -1,10 +1,14 @@
 const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient();
 exports.handler = async function (event)  {
-    var data = await dynamo.scan({ TableName: "test" }).promise();
+    var data = await scanData();
     const response = {
         statusCode: 200,
         body: data,
     };
     return response;
 };
+async function scanData()
+{
+    return await dynamo.scan({ TableName: "test" }).promise();
+}
